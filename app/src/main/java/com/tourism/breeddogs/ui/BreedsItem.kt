0d52605d503c750c817abf6bb364d65071325281
Alselwi <alselwi@Alselwis-MacBook-Pro.local>
2023-07-26 22:37:53 +0100

    Added Icons

--- app/src/main/java/com/tourism/breeddogs/ui/BreedsItem.kt
+++ app/src/main/java/com/tourism/breeddogs/ui/BreedsItem.kt
@@ -1,5 +1,6 @@
 package com.tourism.breeddogs.ui
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,16 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.tourism.breeddogs.R
 import com.tourism.breeddogs.data.DogBreed
 
 @Composable
@@ -37,14 +41,22 @@ fun BreedsItem(
         Card(modifier = Modifier.fillMaxWidth()) {
             breed.let {
 
-                Column(modifier = Modifier.fillMaxSize().clickable {
-                    navController.navigate(Screen.DogDetails.route+"/"+it.name)
-                },
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = it.name, modifier = Modifier.padding(8.dp).clickable {
-                         }, fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        navController.navigate(Screen.DogDetails.route + "/" + it.name)
+                    }) {
+                    Row(modifier = Modifier
+                        .fillMaxSize().padding(9.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = it.name, modifier = Modifier
+                            .padding(9.dp)
+                            .clickable {
+                            }, fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold)
+                        Icon(painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24),
+                            contentDescription = null, modifier = Modifier.padding(9.dp))
+                    }
                 }
             }
 
