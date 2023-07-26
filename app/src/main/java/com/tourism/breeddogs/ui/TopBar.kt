388f462eb22e2d9f5e2cf542531f3dafd2abf282
Alselwi <alselwi@Alselwis-MacBook-Pro.local>
2023-07-26 16:03:11 +0100

    Nav back added

--- app/src/main/java/com/tourism/breeddogs/ui/TopBar.kt
+++ app/src/main/java/com/tourism/breeddogs/ui/TopBar.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -62,5 +66,15 @@ fun TopBar(navController: NavController) {
                     )
                 }
             }
-        })
+        },
+         navigationIcon = {if (navController.previousBackStackEntry != null) {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back", tint = Color.Green
+                )
+            }
+        }
+        }
+    )
 }
